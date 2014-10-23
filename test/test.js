8232da0442058b6d80ce2f142cf557e8f4b8a116
lefebure brian <blefebur@prune>
2014-10-23 15:32:40 +0200

    STORY 1 COMMIT TEST

--- test/test.js
+++ test/test.js
@@ -3,4 +3,24 @@
 var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
+    var paletto = new Engine();
+    paletto.init();
+    paletto.fill();
+    for(var i = 0;i<6;i++){
+        for(var j = 0;j<6;j++){
+            if(i<5){
+                assertTrue(paletto.getBallGrid(i,j) != paletto.getBallGrid(i+1,j));
+            }
+            if(i>0){
+                assertTrue(paletto.getBallGrid(i,j) != paletto.getBallGrid(i-1,j));
+            }
+            if(j>0){
+                assertTrue(paletto.getBallGrid(i,j) != paletto.getBallGrid(i,j-1));
+            }
+            if(j<5){
+                assertTrue(paletto.getBallGrid(i,j) != paletto.getBallGrid(i,j+1));
+            }
+        }
+    }
+
 };
